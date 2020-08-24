47274c3787a29a8d7ebba8199d319c2378ff4fa8
Taylor Fang <stu005606@gmail.com>
2020-08-24 15:27:48 +0800

    refactor(zoom-in): add zoomable-image

--- packages/zoom-in/src/components/zoomable-image.js
+++ packages/zoom-in/src/components/zoomable-image.js
@@ -1,8 +1,11 @@
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useState } from 'react'
 import Zoom from './zoom'
-import styled, { ThemeProvider } from 'styled-components'
+import defaultTheme from '../themes/default-theme'
 import merge from 'lodash/merge'
+import mq from '@twreporter/core/lib/utils/media-query'
+import styled, { ThemeProvider } from 'styled-components'
+import useEventListener from '../hooks/use-event-listener'
 
 const _ = {
   merge,
@@ -18,7 +21,7 @@ const Figure = styled.figure`
   }
 `
 
-const OriginalCaption = styled.figcaption`
+const Caption = styled.figcaption`
   font-size: 12px;
   line-height: 18px;
   letter-spacing: 0.34px;
@@ -28,53 +31,75 @@ const OriginalCaption = styled.figcaption`
   hyphens: auto;
 `
 
-const defaultTheme = {
-  // image: {
-  marginTop: 32,
-  marginLeft: 42,
-  marginRight: 42,
-  marginBottom: 15,
-  // }
-  // overlay: {
-  background: '#fff',
-  opacity: 1,
-  overlayZindex: 1,
-  // }
-  // caption:{
-  captionPosition: 'bottom', // top, right, bottom, left
-  captionAlign: 'left', // left or right; top or bottom
-  captionOffset: 0,
-  captionFontSize: 12,
-  captionLineHeight: 18,
-  captionLetterSpacing: 0.34,
-  captionColor: '#000',
-  captionMarginBottom: 24,
-  captionWidth: 578,
-  // TODO
-  captionFont: '',
-  // }
-  frame: {
-    width: document.documentElement.clientWidth,
-    height: document.documentElement.clientHeight,
-    left: 0,
-    top: 0,
-    right: 0,
-    bottom: 0,
-  },
+const Hd = styled.div`
+  display: none;
+  ${mq.hdOnly`
+    display: block;
+  `}
+`
+const Desktop = styled.div`
+  display: none;
+  ${mq.desktopOnly`
+    display: block;
+  `}
+`
+const Tablet = styled.div`
+  display: none;
+  ${mq.tabletOnly`
+    display: block;
+  `}
+`
+const Mobile = styled.div`
+  display: none;
+  ${mq.mobileOnly`
+    display: block;
+  `}
+`
+
+const setFrame = theme => {
+  const defaultFrame = {
+    frame: {
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight,
+      left: 0,
+      top: 0,
+      right: 0,
+      bottom: 0,
+    },
+  }
+  return _.merge({}, defaultFrame, theme)
 }
 
 const ZoomableImage = ({ src, caption, alt, theme }) => {
-  // TODO: make twreporter style's OriginalCaption
-  if (!src) return null
+  const [, setWindowWidth] = useState(0)
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth)
+  }
+
+  useEventListener(window, 'resize', handleResize)
+
+  const themedImage = customizedTheme => {
+    if (!src) return null
+    const theme = _.merge({}, defaultTheme, setFrame(customizedTheme))
+    return (
+      <ThemeProvider theme={theme}>
+        <Figure>
+          <Zoom caption={caption}>
+            <img src={src} alt={alt} />
+          </Zoom>
+          <Caption>{caption}</Caption>
+        </Figure>
+      </ThemeProvider>
+    )
+  }
+
   return (
-    <ThemeProvider theme={_.merge({}, defaultTheme, theme)}>
-      <Figure>
-        <Zoom caption={caption}>
-          <img src={src} alt={alt} />
-        </Zoom>
-        <OriginalCaption>{caption}</OriginalCaption>
-      </Figure>
-    </ThemeProvider>
+    <React.Fragment>
+      <Hd>{themedImage(theme.hd)}</Hd>
+      <Desktop>{themedImage(theme.desktop)}</Desktop>
+      <Tablet>{themedImage(theme.tablet)}</Tablet>
+      <Mobile>{themedImage(theme.mobile)}</Mobile>
+    </React.Fragment>
   )
 }
 
@@ -82,15 +107,44 @@ ZoomableImage.propTypes = {
   src: PropTypes.string,
   caption: PropTypes.string,
   alt: PropTypes.string,
-  // TODO: to be defined
-  theme: PropTypes.object,
+  theme: PropTypes.shape({
+    image: PropTypes.shape({
+      marginTop: PropTypes.number,
+      marginBottom: PropTypes.number,
+      marginLeft: PropTypes.number,
+      marginRight: PropTypes.number,
+    }),
+    overlay: PropTypes.shape({
+      background: PropTypes.string,
+      opacity: PropTypes.number,
+      zIndex: PropTypes.number,
+    }),
+    caption: PropTypes.shape({
+      marginTop: PropTypes.number,
+      marginBottom: PropTypes.number,
+      marginLeft: PropTypes.number,
+      marginRight: PropTypes.number,
+      fontSize: PropTypes.number,
+      lineHeight: PropTypes.number,
+      letterSpacing: PropTypes.number,
+      color: '#000',
+      fontFamily: PropTypes.string,
+      offset: PropTypes.number,
+    }),
+    zoomOptions: PropTypes.shape({
+      scrollableEl: PropTypes.element,
+      transitionDuration: PropTypes.number,
+      transitionFunction: PropTypes.string,
+      scrollOffset: PropTypes.number,
+    }),
+  }),
 }
 
 ZoomableImage.defaultProps = {
   src: '',
   caption: '',
   alt: '',
-  theme: null,
+  theme: defaultTheme,
 }
 
 export default ZoomableImage
