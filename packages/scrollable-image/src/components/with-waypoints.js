eb3ffe0163c3ee79614819c62ff7d3ad1ae2ddf3
nickhsine <nickhsine97753017@gmail.com>
2020-12-03 19:13:53 +0800

    refactor: make onPositionChange simple

--- packages/scrollable-image/src/components/with-waypoints.js
+++ packages/scrollable-image/src/components/with-waypoints.js
@@ -52,82 +52,110 @@ const withWaypoints = WrappedComponent => {
 
     _handleTopBoundaryPositionChange(position) {
       const { previousPosition, currentPosition } = position
-      const { bottomBoundaryPosition } = this.waypointsPosition
+      // const { bottomBoundaryPosition } = this.waypointsPosition
+      // this._setWaypointPosition({ topBoundaryPosition: currentPosition })
 
-      this._setWaypointPosition({ topBoundaryPosition: currentPosition })
-
-      // top boundary enter to viewport
-      if (currentPosition === Waypoint.inside) {
-        if (previousPosition === Waypoint.above) {
-          this.setScrollState({
-            isActive: false,
-            childrenAligned: 'top',
-          })
-        } else if (previousPosition === Waypoint.below) {
-          this.setScrollState({
-            isActive: false,
-            childrenAligned: 'top',
-          })
-        }
+      if (
+        (previousPosition === Waypoint.inside ||
+          previousPosition === Waypoint.below) &&
+        currentPosition === Waypoint.above
+      ) {
+        this.setScrollState({
+          isActive: true,
+        })
         return
       }
+      this.setScrollState({
+        isActive: false,
+        childrenAligned: 'top',
+      })
 
-      // top boundary leave from viewport
-      if (previousPosition === Waypoint.inside) {
-        if (currentPosition === Waypoint.above) {
-          if (bottomBoundaryPosition === Waypoint.inside) return
-          this.setScrollState({
-            isActive: true,
-          })
-        } else if (currentPosition === Waypoint.below) {
-          this.setScrollState({
-            isActive: false,
-            childrenAligned: 'top',
-          })
-        }
-      }
+      /// / top boundary enter to viewport
+      // if (currentPosition === Waypoint.inside) {
+      //  if (previousPosition === Waypoint.above) {
+      //    this.setScrollState({
+      //      isActive: false,
+      //      childrenAligned: 'top',
+      //    })
+      //  } else if (previousPosition === Waypoint.below) {
+      //    this.setScrollState({
+      //      isActive: false,
+      //      childrenAligned: 'top',
+      //    })
+      //  }
+      //  return
+      // }
+
+      /// / top boundary leave from viewport
+      // if (previousPosition === Waypoint.inside) {
+      //  if (currentPosition === Waypoint.above) {
+      //    if (bottomBoundaryPosition === Waypoint.inside) return
+      //    this.setScrollState({
+      //      isActive: true,
+      //    })
+      //  } else if (currentPosition === Waypoint.below) {
+      //    this.setScrollState({
+      //      isActive: false,
+      //      childrenAligned: 'top',
+      //    })
+      //  }
+      // }
     }
 
     _handleBottomBoundaryPositionChange(position) {
       const { previousPosition, currentPosition } = position
-      const { topBoundaryPosition } = this.waypointsPosition
+      // const { topBoundaryPosition } = this.waypointsPosition
+      // this._setWaypointPosition({ bottomBoundaryPosition: currentPosition })
 
-      this._setWaypointPosition({ bottomBoundaryPosition: currentPosition })
-
-      // bottom boundary enter to viewport
-      if (currentPosition === Waypoint.inside) {
-        if (previousPosition === Waypoint.below) {
-          // bottom boundary enter from below
-          this.setScrollState({
-            isActive: false,
-            childrenAligned: 'bottom',
-          })
-        } else if (previousPosition === Waypoint.above) {
-          // bottom boundary enter from above
-          this.setScrollState({
-            isActive: false,
-            childrenAligned: 'bottom',
-          })
-        }
+      if (
+        (previousPosition === Waypoint.inside ||
+          previousPosition === Waypoint.above) &&
+        currentPosition === Waypoint.below
+      ) {
+        this.setScrollState({
+          isActive: true,
+        })
         return
       }
+      this.setScrollState({
+        isActive: false,
+        childrenAligned: 'bottom',
+      })
 
-      // bottom boundary leave from viewport
-      if (previousPosition === Waypoint.inside) {
-        if (currentPosition === Waypoint.below) {
-          // bottom boundary leave to below
-          if (topBoundaryPosition === Waypoint.inside) return
-          this.setScrollState({
-            isActive: true,
-          })
-        } else if (currentPosition === Waypoint.above) {
-          // bottom boundary leave to above
-          this.setScrollState({
-            isActive: false,
-            childrenAligned: 'bottom',
-          })
-        }
-      }
+      /// / bottom boundary enter to viewport
+      // if (currentPosition === Waypoint.inside) {
+      //  if (previousPosition === Waypoint.below) {
+      //    // bottom boundary enter from below
+      //    this.setScrollState({
+      //      isActive: false,
+      //      childrenAligned: 'bottom',
+      //    })
+      //  } else if (previousPosition === Waypoint.above) {
+      //    // bottom boundary enter from above
+      //    this.setScrollState({
+      //      isActive: false,
+      //      childrenAligned: 'bottom',
+      //    })
+      //  }
+      //  return
+      // }
+
+      /// / bottom boundary leave from viewport
+      // if (previousPosition === Waypoint.inside) {
+      //  if (currentPosition === Waypoint.below) {
+      //    // bottom boundary leave to below
+      //    if (topBoundaryPosition === Waypoint.inside) return
+      //    this.setScrollState({
+      //      isActive: true,
+      //    })
+      //  } else if (currentPosition === Waypoint.above) {
+      //    // bottom boundary leave to above
+      //    this.setScrollState({
+      //      isActive: false,
+      //      childrenAligned: 'bottom',
+      //    })
+      //  }
+      // }
     }
 
     render() {
